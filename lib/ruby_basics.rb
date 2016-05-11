ba827a80e8dcb708467105f24f65aa5b6fec9c73
Zeeshan Mustafa <zeeshan.mustafa@yahoo.com>
2016-05-10 22:13:55 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ 42/7
 end
 
 def assign_variable(value)
-  
+value="Bob"
 end
 
-def argue
-  
+def argue(phrase)
+phrase = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting(greeting,name)
+  greeting = "Hi there, "
+  name = "Bobby!"
 end
 
 def return_a_value
-
+return_a_value="Nice"
 end
 
 def last_evaluated_value
-
+last_evaluated_value="expert"
 end
 
-def pizza_party
-
+def pizza_party(pizza_party="cheese")
+pizza_party
 end
